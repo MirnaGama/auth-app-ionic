84612164b320f499bba2b49a31b80f7bd06b463a
mirnagama <mirnabeatrizgamav@gmail.com>
2022-02-01 15:57:15 -0300

    Form Validation - Login Page

--- src/app/public/login/login.page.ts
+++ src/app/public/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +9,33 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private authService: AuthenticationService) { }
+  private loginForm : FormGroup;
+
+  constructor(private authService: AuthenticationService, private formBuilder: FormBuilder) {
+   }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  get f() {
+    return this.loginForm.controls;
+  }
+
+  createForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
   login() {
+    this.loginForm.markAllAsTouched();
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
     this.authService.login();
   }
 
